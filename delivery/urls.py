e54236307e1ce1f3fb2bdf61ffea816115d393b1
Ahmad Mukhtar <ahmadmukhtar2032@gmail.com>
2022-12-05 21:08:31 +0100

    Update urls.py

--- delivery/urls.py
+++ delivery/urls.py
@@ -18,9 +18,11 @@ from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
+from homee.views import Home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', Home),
     path('', include('homee.urls')),
 ]
 
